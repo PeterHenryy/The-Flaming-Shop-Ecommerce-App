ebab0f2a6ddc5606bff7bd279ce35af4ee9d13ee
PeterHenryy <snakenoxz@gmail.com>
2023-07-07 17:42:21 -0300

    010- Added Company model.

---
+++ EcommerceApp1/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp1.Models
+{
+    public class Company
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ProductsInStock { get; set; }
+        public string Logo { get; set; }
+        public int TotalSales { get; set; }
+        public double Revenue { get; set; }
+    }
+}
