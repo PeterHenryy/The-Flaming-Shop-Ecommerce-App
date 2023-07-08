83136f170056fc37738fde061b10e19ccff7747b
PeterHenryy <snakenoxz@gmail.com>
2023-07-07 22:13:41 -0300

    060- Added App User functionality

---
+++ EcommerceApp1/Helpers/Enums/UserRolesEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApp1.Helpers.Enums
+{
+    public enum UserRolesEnum
+    {
+        Customer = 0,
+        Admin = 1
+    }
+}
---
+++ EcommerceApp1/Models/Identity/AppLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceApp1.Models.Identity
+{
+    public class AppLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
