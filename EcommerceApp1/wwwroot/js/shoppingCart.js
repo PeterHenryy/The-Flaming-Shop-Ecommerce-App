ead35401784040b91717e506eed193f49d7b846b
PeterHenryy <snakenoxz@gmail.com>
2023-10-08 21:41:23 -0300

    420- Added more styling to Shopping Cart page

--- EcommerceApp1/wwwroot/js/shoppingCart.js
+++ EcommerceApp1/wwwroot/js/shoppingCart.js
@@ -13,6 +13,22 @@
     toastr.success(`Added ${productName} to Cart! (x${quantity})`);
 }
 
+calculateOrderSubtotal();
+displayOrderItems();
+calculateTax();
+calculateOrderTotal();
+
+function updateItemsAndPrices(productID, quantity, price) {
+    updateItemQuantityInCheckout(productID, quantity);
+    updateItemQuantityHTML(productID);
+    updateProductTotalPrice(productID, quantity, price);
+    calculateOrderSubtotal();
+    displayOrderItems();
+    calculateTax();
+    calculateShipping(productID);
+    calculateOrderTotal();
+}
+
 function updateCartItemQuantity(quantity) {
     const cartItemQuantityElement = document.querySelector('.cart-item-quantity');
     let itemQuantity = Number(cartItemQuantityElement.innerHTML);
@@ -52,7 +68,12 @@ function updateItemQuantityInCheckout(itemID, quantity) {
     })
 
 }
-
+function clearCart() {
+    const orderSummaryContainer = document.querySelector('.order-summary-container');
+    const clearCartButton = document.querySelector('.clear-cart');
+    orderSummaryContainer.remove();
+    clearCartButton.innerHTML = '<h1>There are no items in your cart!</h1>';
+}
 function removeFromCart(itemID, productName) {
     const data = {
         itemID
@@ -68,5 +89,85 @@ function removeFromCart(itemID, productName) {
 
     const cartProductElement = document.querySelector(`.js-cart-product-${itemID}`)
     cartProductElement.remove();
-    updateItemQuantityHTML(itemID);
+    const cartProducts = document.querySelectorAll('.cart-product');
+    if (cartProducts.length === 0) {
+        clearCart();
+        updateItemQuantityHTML(itemID);
+    }
+    else {
+        updateItemQuantityHTML(itemID);
+        displayOrderItems();
+        calculateOrderSubtotal();
+        calculateTax();
+        calculateShipping(itemID);
+        calculateOrderTotal();
+    }
 }
+
+function calculateOrderSubtotal() {
+    const summaryPrice = document.querySelector('.js-subtotal-price');
+    const productPrices = document.querySelectorAll('.product-total');
+    let orderTotalPrice = 0;
+    productPrices.forEach(price => {
+        orderTotalPrice += Number(price.innerHTML);
+    });
+    summaryPrice.innerHTML = orderTotalPrice.toFixed(2);
+}
+
+function displayOrderItems() {
+    const subtotalItemsElement = document.querySelector('.js-subtotal-items');
+    const cartItemQuantityElement = document.querySelector('.cart-item-quantity');
+    const subtotalItems = cartItemQuantityElement.innerHTML;
+    subtotalItemsElement.innerHTML = `Subtotal (${subtotalItems} items)`;
+}
+
+function calculateTax() {
+    const taxPercentage = 8;
+    const subtotal = document.querySelector('.js-subtotal-price');
+    const formatedTotal = (Number(subtotal.innerHTML) * 100) / 100;
+    const tax = Math.round((formatedTotal * (taxPercentage / 100)) * 100) / 100;
+    const taxElement = document.querySelector('.tax-cost');
+    taxElement.innerHTML = tax.toFixed(2);
+}
+
+function calculateShipping(productID) {
+    const shippingElement = document.querySelector('.shipping-cost');
+    const selectedShipping = document.querySelectorAll('.js-delivery-option');
+    let shippingCost = 0;
+    selectedShipping.forEach(element => {
+        if (element.checked) {
+            shippingCost += Number(element.value);
+        }
+    });
+    shippingElement.innerHTML = (shippingCost === 0) ? "FREE" : `$${shippingCost}`;
+    calculateOrderTotal();
+}
+
+function calculateOrderTotal() {
+    const summaryPrices = document.querySelectorAll('.js-summary-price');
+    const orderFinalPriceElement = document.querySelector('.order-total-price');
+    let finalPrice = 0;
+    summaryPrices.forEach(price => {
+        if (price.innerHTML !== "FREE") {
+            let fixedPrice = price.innerHTML.replace('$', '');
+            finalPrice += Number(fixedPrice);
+        }
+    });
+
+    const formattedFinalPrice = (finalPrice * 100) / 100;
+    orderFinalPriceElement.innerHTML = `$${formattedFinalPrice.toFixed(2)}`;
+}
+
+$(document).ready(function () {
+    const couponButton = $(".coupon-button");
+    const couponInput = $(".coupon-input");
+    const submitCoupon = $("#submit-coupon");
+    couponButton.on("click", () => {
+        couponInput.slideToggle("slow");
+        submitCoupon.slideToggle("slow");
+    });
+    submitCoupon.on("click", () => {
+        couponInput.slideToggle("slow");
+        submitCoupon.slideToggle("slow");
+    });
+});
